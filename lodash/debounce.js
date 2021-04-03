0ba3205964cf817eafe4c0b401b26f94947f014a
JX-Zhuang <who.zhuang@gmail.com>
2021-04-03 23:26:56 +0800

    debounce

--- lodash/debounce.js
+++ lodash/debounce.js
@@ -1,4 +1,12 @@
-function debounce() {
-    return 123
+function debounce(func, wait = 0, options = {}) {
+    var t = null;
+    return function (...args) {
+        if (t) return;
+        t = setTimeout(() => {
+            func(...args);
+            clearInterval(t);
+            t = null;
+        }, wait);
+    }
 }
 export default debounce;
